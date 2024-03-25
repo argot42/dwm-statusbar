fb1e1bf83718e8f56b9471d68cd337a368c0e25a
argot42 <pablotesari@gmail.com>
2024-03-25 01:34:36 -0300

    removed awful vol scripts for better one for alsa

--- linux/voldown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sink="alsa_output.pci-0000_00_1f.3.analog-stereo"
-
-pactl -- set-sink-volume "$sink" "-5%"
-
-writevol.sh
--- linux/writevol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sink="alsa_output.pci-0000_00_1f.3.analog-stereo"
-pipe_path="/tmp/dwm_status.fifo"
-
-line=$(pactl list sinks | grep -n "Name: $sink" | cut -d : -f1)
-volume=$(pactl list sinks | sed "$((line + 7))q;d" | awk '/\/ /{print $5}')
-
-echo "V$volume" > $pipe_path
